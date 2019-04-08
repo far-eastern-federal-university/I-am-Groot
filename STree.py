ad7f2ca794ca2057c71cace20700cbf4d71553a3
Unknown <zhildemon@gmail.com>
2019-04-08 17:25:47 +1000

    titanic tree

--- STree.py
+++ STree.py
@@ -3,3 +3,34 @@
 import scipy as scp
 import numpy as np
 import pandas as pd
+import os
+from sklearn.tree import DecisionTreeClassifier
+
+# Test (coursera)
+
+X = np.array([[1, 2], [3, 4], [5, 6]])
+y = np.array([0, 1, 0])
+
+clf = DecisionTreeClassifier()
+clf.fit(X, y)
+
+importances = clf.feature_importances_
+print(importances)
+
+# Titanic (coursera)
+
+data = pd.read_csv("titanic.csv", sep = ",")
+print(data.describe()) # check if we've loaded right data
+
+data_notna = pd.DataFrame.dropna(data)
+print(data_notna.describe())
+
+X = data_notna[["Pclass", "Fare", "Age", "Sex"]]
+X.replace("male", 0, True, None, False)
+X.replace("female", 1, True, None, False)
+print(X.describe)
+
+y = data_notna["Survived"]
+
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(X, y)
