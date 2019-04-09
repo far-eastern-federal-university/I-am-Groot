b2be723bcb81f56221077f83d2dece0ac6384ba0
Unknown <zhildemon@gmail.com>
2019-04-09 16:44:02 +1000

    predict survived
    titanic data set: learn, predict, cross-validate

--- STree.py
+++ STree.py
@@ -1,10 +1,14 @@
 # Simple decision tree
 
-import scipy as scp
 import numpy as np
 import pandas as pd
 import os
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+import matplotlib.pyplot as plt
+
 
 # Test (coursera)
 
@@ -28,9 +32,44 @@ print(data_notna.describe())
 X = data_notna[["Pclass", "Fare", "Age", "Sex"]]
 X.replace("male", 0, True, None, False)
 X.replace("female", 1, True, None, False)
-print(X.describe)
+print(X[:5])
 
 y = data_notna["Survived"]
 
 clf = DecisionTreeClassifier(random_state=241)
 clf.fit(X, y)
+
+print("Do they survived?")
+n = 1
+print(clf.predict(X[:5]))
+
+print(clf.feature_importances_)
+print(cross_val_score(clf, X, y, cv=10))
+
+plt.figure()
+plt.title("tree")
+plt.xlabel("Training examples")
+plt.ylabel("Score")
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=241)
+train_sizes=np.linspace(.1, 1.0, 5)
+train_sizes, train_scores, test_scores = learning_curve(
+        clf, X, y, cv=cv, n_jobs=4, train_sizes=train_sizes)
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+plt.grid()
+plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+
+plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+plt.legend(loc="best")
+
+plt.show()
